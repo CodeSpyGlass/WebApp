83e2ccf44bec2af4aea97e99ede2e9be5bb6f8ae
romo <robmoore121@gmail.com>
2021-01-02 14:01:01 +0800

    Update test name

--- src/App.test.tsx
+++ src/App.test.tsx
@@ -2,7 +2,7 @@ import React from 'react';
 import { render, screen } from '@testing-library/react';
 import App from './App';
 
-test('renders learn react link', () => {
+test('renders prompt', () => {
   render(<App />);
   const prompt = screen.getByText(/Enter a GitHub repository URL/i);
   expect(prompt).toBeInTheDocument();
